e230732bd5b668ac98437c44d6b4c1b20e0e8ce5
danilka <denchiksrty@gmail.com>
2023-04-26 18:57:21 +0400

    final1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, abort, request
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
@@ -15,6 +17,8 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.register_blueprint(news_api.blueprint)
     app.run()
 
